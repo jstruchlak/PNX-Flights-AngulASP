67597b3ee8a86ab95293f157a28b7fea3c856883
Struchlak, Joey (ECSA) <Joey.Struchlak@sa.gov.au>
2024-12-02 15:30:47 +1030

    Added error handling inside API and Client side, Swagger documentation to show status codes

--- angasppnx.client/src/app/book-flight/book-flight.component.ts
+++ angasppnx.client/src/app/book-flight/book-flight.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import {FlightService } from '../api/services/flight.service'
+import {FlightRm } from '../api/models/flight-rm'
 
 @Component({
   selector: 'app-book-flight',
@@ -9,9 +10,13 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class BookFlightComponent implements OnInit{
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+    private flightService: FlightService,
+    private router: Router
+  ) { }
 
   flightId: string = 'not loaded'
+  flight: FlightRm = {}
 
   ngOnInit(): void {
     this.route.paramMap
@@ -21,6 +26,22 @@ export class BookFlightComponent implements OnInit{
 
   private findFlight = (flightId: string | null) => {
     this.flightId = flightId ?? 'not passed';
+
+    this.flightService.findFlight({ id: this.flightId })
+      .subscribe(flight => this.flight = flight,
+      this.handleError)
+  }
+
+  private handleError = (err: any) => {
+
+    if (err.status == 404) {
+      alert("Flight Not Found")
+      this.router.navigate(['/search-flights'])
+    }
+
+    console.log("Response Error. Status: ", err.status)
+    console.log("Response Error. Status Text: ", err.statusText)
+    console.log(err)
   }
 
 }
