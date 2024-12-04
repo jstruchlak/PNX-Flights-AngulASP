20476d48f6650d1a300418b39af034d6ae07a66d
Struchlak, Joey (ECSA) <Joey.Struchlak@sa.gov.au>
2024-12-04 12:55:46 +1030

    Added auth service and some styling and DTO's, routing

--- angasppnx.client/src/app/app.component.ts
+++ angasppnx.client/src/app/app.component.ts
@@ -1,7 +1,9 @@
+import { style } from '@angular/animations';
 import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
--- angasppnx.client/src/app/book-flight/book-flight.component.ts
+++ angasppnx.client/src/app/book-flight/book-flight.component.ts
@@ -1,47 +1,90 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import {FlightService } from '../api/services/flight.service'
-import {FlightRm } from '../api/models/flight-rm'
+import { FlightService } from '../api/services/flight.service';
+import { FlightRm } from '../api/models/flight-rm';
+import { AuthService } from '../auth/auth.service';
+import { FormBuilder } from '@angular/forms';
+import { BookDto } from '../api/models/book-dto';
 
 @Component({
   selector: 'app-book-flight',
   templateUrl: './book-flight.component.html',
-  styleUrls: ['./book-flight.component.css']
+  styleUrls: ['./book-flight.component.css'],
 })
-export class BookFlightComponent implements OnInit{
+export class BookFlightComponent implements OnInit {
+  flightId: string = 'not loaded';
+  flight: FlightRm = {};
+  form = this.fb.group({
+    number: [1],
+  });
 
-  constructor(private route: ActivatedRoute,
+  constructor(
+    private route: ActivatedRoute,
     private flightService: FlightService,
-    private router: Router
+    private router: Router,
+    private authService: AuthService,
+    private fb: FormBuilder
   ) { }
 
-  flightId: string = 'not loaded'
-  flight: FlightRm = {}
-
   ngOnInit(): void {
-    this.route.paramMap
-      .subscribe(p => this.findFlight(p.get("flightId")))
-
-  }
-
-  private findFlight = (flightId: string | null) => {
-    this.flightId = flightId ?? 'not passed';
-
-    this.flightService.findFlight({ id: this.flightId })
-      .subscribe(flight => this.flight = flight,
-      this.handleError)
-  }
-
-  private handleError = (err: any) => {
-
-    if (err.status == 404) {
-      alert("Flight Not Found")
-      this.router.navigate(['/search-flights'])
+    if (!this.authService.currentUser) {
+      this.router.navigate(['/register-passenger']);
+      return;
     }
 
-    console.log("Response Error. Status: ", err.status)
-    console.log("Response Error. Status Text: ", err.statusText)
-    console.log(err)
+    this.route.paramMap.subscribe((p) => {
+      const flightId = p.get('flightId');
+      if (!flightId) {
+        console.error('Flight ID is missing.');
+        this.router.navigate(['/search-flights']);
+        return;
+      }
+      this.findFlight(flightId);
+    });
   }
 
+  private findFlight(flightId: string): void {
+    this.flightId = flightId;
+
+    this.flightService.findFlight({ id: this.flightId }).subscribe(
+      (flight) => (this.flight = flight),
+      (err) => this.handleError(err)
+    );
+  }
+
+  private handleError(err: any): void {
+    if (err.status === 404) {
+      alert('Flight Not Found');
+      this.router.navigate(['/search-flights']);
+    }
+
+    console.error('Response Error:', {
+      status: err.status,
+      statusText: err.statusText,
+      error: err,
+    });
+  }
+
+  book(): void {
+    const numberOfPassengers = this.form.get('number')?.value;
+
+    if (!numberOfPassengers || typeof numberOfPassengers !== 'number') {
+      console.error('Invalid number of passengers.');
+      return;
+    }
+
+    console.log(
+      `Booking ${numberOfPassengers} passengers for the flight ${this.flightId}`
+    );
+
+    const booking: BookDto = {
+      flightId: this.flight.id,
+      passengerEmail: this.authService.currentUser?.email,
+      numberOfSeats: this.form.get('number')?.value ?? undefined
+    }
+
+    this.flightService.bookFlight({ body: booking })
+      .subscribe(_ => this.router.navigate(['/my-booking']),
+        this.handleError);
+  }
 }
